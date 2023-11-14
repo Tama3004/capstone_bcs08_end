6db69a8e58a7e7cd39c5ddc5ea260d0a434d46a8
Khoahello <dinhtankhoa2k7@gmail.com>
2023-11-15 00:32:45 +0700

    update delete job da thue

--- src/page/Profile/ListJob.js
+++ src/page/Profile/ListJob.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { https } from "../../api/config";
 import { userLocalStorage } from "../../api/localService";
 import { NavLink } from "react-router-dom";
+import { message } from "antd";
 
 export default function ListJob() {
   let { userLogin } = useSelector((state) => {
@@ -11,24 +12,46 @@ export default function ListJob() {
 
   let [listJobThue, setListJobThue] = useState(null);
 
+  const getListJobThue = () => {
+    https
+      .get("/api/thue-cong-viec/lay-danh-sach-da-thue", {
+        headers: {
+          token: userLocalStorage.get().token,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setListJobThue(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     let fetchData = () => {
-      https
-        .get("/api/thue-cong-viec/lay-danh-sach-da-thue", {
-          headers: {
-            token: userLocalStorage.get().token,
-          },
-        })
-        .then((res) => {
-          console.log(res);
-          setListJobThue(res.data.content);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      getListJobThue();
     };
     fetchData();
   }, []);
+
+  let handleDeleteJob = (id) => {
+    https
+      .delete(`/api/thue-cong-viec/${id}`, {
+        headers: {
+          token: userLocalStorage.get().token,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        getListJobThue();
+        message.success("Xóa công việc thành công!");
+      })
+      .catch((err) => {
+        console.log(err);
+        message.error(`${err.message}`);
+      });
+  };
 
   let renderItem = () => {
     return listJobThue?.map((item, index) => {
@@ -68,7 +91,12 @@ export default function ListJob() {
               </button>
             </NavLink>
             <div class="right">
-              <button class="delete bg-red-500 text-white font-bold rounded transition-all hover:shadow-lg px-3 py-2">
+              <button
+                class="delete bg-red-500 text-white font-bold rounded transition-all hover:shadow-lg px-3 py-2"
+                onClick={() => {
+                  handleDeleteJob(item.id);
+                }}
+              >
                 DEL
               </button>
             </div>
